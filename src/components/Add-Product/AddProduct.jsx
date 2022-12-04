a9d42d5e22934ccf5f9c4ac3c594126bec57ad06
Carlos Ruiz <carlosbustillo99@gmail.com>
2022-12-04 00:04:06 -0600

    [add] module product item

--- src/components/Add-Product/AddProduct.jsx
+++ src/components/Add-Product/AddProduct.jsx
@@ -1,28 +1,31 @@
-import React, { useContext } from 'react'
-import AppContext from '../../context/AppContext';
+import React, { useContext } from "react";
+import AppContext from "../../context/AppContext";
 import { Button } from "primereact/button";
-import './AddProduct.scss'
-import ItemProduct from './Item-Product';
+import "./AddProduct.scss";
+import ItemProduct from "./Item-Product";
 
-const AddProduct = ({products, hideModal}) => {
-    const {addToCart} = useContext(AppContext);
+const AddProduct = ({ products, hideModal }) => {
+  const { addToCart } = useContext(AppContext);
 
-    const handleClick = item =>{
-        addToCart(item);
-    };
-    console.table(products);
-    return (
-      <div className="form-add-product">
-        {Object.entries(products).map((item, idx) => (
-          <ItemProduct key={idx} products={item} />
-        ))}
+  const handleClick = (item) => {
+    addToCart(item);
+  };
+  console.table(products);
+  return (
+    <div id="form-add-product" className="form-add-product">
+      {products
+        .map((item, idx) => <ItemProduct key={idx} products={item} />)
+        .slice(0, 1)}
+      <div className="form-button-group">
+        <Button label="Borrar" className="p-button-danger" />
         <Button
           icon="pi pi-shopping-cart"
-          label="Add to Cart"
+          label="Agregar al carrito"
           onClick={() => handleClick(products)}
         ></Button>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default AddProduct;
--- src/components/Add-Product/Item-Product.jsx
+++ src/components/Add-Product/Item-Product.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
+import logo from "../../assets/logo.png";
 
-const ItemProduct = ({products, idx}) => {
+const ItemProduct = ({ products, idx }) => {
 
-    const photoStyle = {
-        width: "3.5rem",
-        height: "auto",
-        borderRadius: "50%",
-      };
+  const photoStyle = {
+    width: "13rem",
+    height: "auto",
+    borderRadius: "50%",
+  };
   return (
-    <form>
-      <image src={products.image} style={photoStyle}/>
-      <span className="name-product">{products.name}</span>
-      <span className='description-product'>{products.price}</span>
+    <form className="form-product">
+      <div className="logo-div">
+        <img src={logo} alt="Logo" style={photoStyle} />
+      </div>
+      <div className="form-group">
+        <span className="name-product">{products.name}</span>
+        <span className="description-product">hola{products.description}</span>
+        <span className="price-product">precio{products.price}</span>
+        <span className="description-product">Cantidad{products.cant}</span>
+      </div>
     </form>
   );
-}
+};
 
 export default ItemProduct;
