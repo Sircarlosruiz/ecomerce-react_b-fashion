3bc75bf5ffc2476b90ebb0fb91aed46c0317329f
AmadorCarlos <carlos.amador450@hotmail.com>
2022-12-08 22:30:03 -0600

    using axios to user context

--- src/context/user.context.js
+++ src/context/user.context.js
@@ -1,8 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
 
 const newUser = (User, user) => {
-  
-  return [...User, { ...user}];
+
+  return [...User, { ...user }];
 };
 
 const deleteItem = (User, user) => {
@@ -12,37 +14,47 @@ const deleteItem = (User, user) => {
 export const UserContext = createContext({
   isUserOpen: false,
   User: [],
-  total: 0,
-  setIsUserOpen: () => {},
-  addUser: () => {},
-  deleteUser: () => {},
-  resetUser: () => {},
+  setIsUserOpen: () => { },
+  addUser: () => { },
+  deleteUser: () => { },
+  resetUser: () => { },
 });
 
-export const UserContextProvider = ({ children }) => {
+export const UserProvider = ({ children }) => {
   const [isUserOpen, setIsUserOpen] = useState(false);
   const [User, setUser] = useState([]);
 
+  useEffect(() => {
+    const getUser = async () => {
+      const URL = "http://localhost:8181/api/client";
+      const response = await axios.get(`${URL}/list`);
+      setUser(response.data);
+    };
+
+    getUser();
+  }, [])
+
   const addUser = (user) => {
-    setUser(newUser(User, user));
-  };
+    setUser(newUser());
+}
 
-  const deleteUser = (item) => {
-    setUser(deleteItem());
-  };
-
-  const resetUser = (user) => {
-    setUser(deleteItem(User, user));
-  };
-
-  const value = {
-    isUserOpen,
-    setIsUserOpen,
-    User,
-    addUser,
-    deleteUser,
-    resetUser,
-  };
-
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+const deleteUser = (item) => {
+  setUser(deleteItem());
 };
+
+const resetUser = (user) => {
+  setUser(deleteItem(User, user));
+};
+
+
+const value = {
+  isUserOpen,
+  User,
+  addUser,
+  setIsUserOpen,
+  deleteUser,
+  resetUser,
+};
+
+return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+  };
