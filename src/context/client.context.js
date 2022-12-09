99b324849f5e82bb92159d69be164596595ecdc3
Carlos Ruiz <carlosbustillo99@gmail.com>
2022-12-09 16:05:32 -0600

    [add] final commit

--- src/context/client.context.js
+++ src/context/client.context.js
@@ -1,9 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
-
 const newUser = (User, user) => {
-
   return [...User, { ...user }];
 };
 
@@ -14,10 +12,10 @@ const deleteItem = (User, user) => {
 export const ClientContext = createContext({
   isClientOpen: false,
   Client: [],
-  setIsClientOpen: () => { },
-  addClient: () => { },
-  deleteClient: () => { },
-  resetUser: () => { },
+  setIsClientOpen: () => {},
+  addClient: () => {},
+  deleteClient: () => {},
+  resetUser: () => {},
 });
 
 export const ClientProvider = ({ children }) => {
@@ -25,36 +23,45 @@ export const ClientProvider = ({ children }) => {
   const [Client, setClient] = useState([]);
 
   useEffect(() => {
+    const URL = "http://localhost:8181/api/client";
     const getUser = async () => {
-      const URL = "http://localhost:8181/api/client";
       const response = await axios.get(`${URL}/list`);
       setClient(response.data);
     };
-
+    
     getUser();
-  }, [])
+  }, []);
+  
+  useEffect(() => {
+    const PostUser = async () => {
+      const sent = await axios.post(`${URL}/save`);
+      addUser(sent.data);
+    };
+    PostUser();
+  }, [Client]);
 
   const addUser = (user) => {
     setClient(newUser());
-}
-
-const deleteUser = (item) => {
-  setClient(deleteItem());
-};
-
-const resetUser = (user) => {
-  setClient(deleteItem(Client, user));
-};
-
-
-const value = {
-  isClientOpen,
-  Client,
-  setIsClientOpen,
-  addUser,
-  deleteUser,
-  resetUser,
-};
-
-return <ClientContext.Provider value={value}>{children}</ClientContext.Provider>;
   };
+
+  const deleteUser = (item) => {
+    setClient(deleteItem());
+  };
+
+  const resetUser = (user) => {
+    setClient(deleteItem(Client, user));
+  };
+
+  const value = {
+    isClientOpen,
+    Client,
+    setIsClientOpen,
+    addUser,
+    deleteUser,
+    resetUser,
+  };
+
+  return (
+    <ClientContext.Provider value={value}>{children}</ClientContext.Provider>
+  );
+};
